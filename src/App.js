8289527ff37b5d2a43abd9a6cfe0989c6827a52e
Omar Sallam <omar.sallam@gmail.com>
2022-01-27 01:18:28 -0500

    Testing first image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import logo from './logo.svg';
+import rom from './Photos/rom.jpg';
+//import rom from './Photos/rom.jpg';
+//import rogers from './Photos/rogers-center.jpg';
+//import oldHall from './Photos/old-city-hall.jpg';
+//import cityHall from './Photos/new-city-hall.jpg';
+//import skyLine from './Photos/city-skyline.jpg';
+//import cnTower from './Photos/cn-tower.jpg';
 import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const images = [logo, logo, logo, logo, logo, logo];
+  const images = [rom, logo, logo, logo, logo, logo];
   const [selectedImage, setSelectedImage] = useState(null);
 
   return (
